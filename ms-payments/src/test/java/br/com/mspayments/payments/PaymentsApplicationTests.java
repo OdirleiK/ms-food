1a18d6b36c695aee3784145cf8a8ff77b197994d
OdirleiK <odirlei67777@gmail.com>
2023-11-12 18:59:25 -0300

    init project

---
+++ ms-payments/src/test/java/br/com/mspayments/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mspayments.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
