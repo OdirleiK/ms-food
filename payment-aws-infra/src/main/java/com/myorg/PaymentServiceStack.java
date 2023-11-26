e7ddc78efb3fe7f5ab30207679642db187da7e58
OdirleiK <odirlei67777@gmail.com>
2023-11-26 10:49:58 -0300

    Configured ECS and CloudWatch

--- payment-aws-infra/src/main/java/com/myorg/PaymentServiceStack.java
+++ payment-aws-infra/src/main/java/com/myorg/PaymentServiceStack.java
@@ -4,14 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecr.IRepository;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 public class PaymentServiceStack extends Stack {
@@ -28,19 +34,29 @@ public class PaymentServiceStack extends Stack {
         				  + ":3306/food-order-ms?createDatabaseIfNotExist=true");
         authentication.put("SPRING_DATASOURCE_USERNAME", "admin");  
         authentication.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue("order-db-senha"));
-        		
+        			
+        
+        IRepository repository = Repository.fromRepositoryName(this, "repository", "img-order-ms");
+
         ApplicationLoadBalancedFargateService.Builder.create(this, "FoodService")
         .serviceName("food-service-ola")
         .cluster(cluster)           // Required
         .cpu(512)                   // Default is 256
-        .desiredCount(1)            // Default is 1
+        .desiredCount(3)            // Default is 1
         .listenerPort(8080)
         .assignPublicIp(true)
         .taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder()
-                          .image(ContainerImage.fromRegistry("kmpx/order-ms"))
+                          .image(ContainerImage.fromEcrRepository(repository))
                           .containerPort(8080)
                           .containerName("order-ms")
                           .environment(authentication)
+                          .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                  .logGroup(LogGroup.Builder.create(this, "OrderMsLogGroup")
+                                      .logGroupName("OrderMsLog")
+                                      .removalPolicy(RemovalPolicy.DESTROY)
+                                      .build())
+                                  .streamPrefix("PedidosMS")
+                                  .build()))
                           .build())
         .memoryLimitMiB(1024)       // Default is 512
         .publicLoadBalancer(true)   // Default is false
